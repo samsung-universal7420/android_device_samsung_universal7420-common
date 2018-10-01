134b196661b2031ec7ba014642ce9898a6c54e9e
Stricted <info@stricted.net>
2018-10-01 10:44:33 +0200

    universal7420: Add autogenerated HIDL manifest
    Change-Id: Id33da4bd1227b36d427bce4cf8f5a594713882cb

--- device-common.mk
+++ device-common.mk
@@ -102,6 +102,10 @@ PRODUCT_PACKAGES += \
     android.hardware.health@2.0-impl \
     android.hardware.health@2.0-service
 
+# HIDL Manifest
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/manifest.xml:$(TARGET_COPY_OUT_VENDOR)/manifest.xml
+
 # Keymaster
 PRODUCT_PACKAGES += \
     keystore.exynos5 \
