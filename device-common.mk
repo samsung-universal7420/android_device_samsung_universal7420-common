81b9fb60ccb3925c4a213ebba8aa88a3ef059267
Stricted <info@stricted.net>
2018-10-01 11:13:05 +0200

    universal7420: Build power hal
    Change-Id: Ie8a4b190f53858feb89c942f6e6c9cff4a348985

--- device-common.mk
+++ device-common.mk
@@ -152,6 +152,12 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nxp.mifare.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
 
+# Power
+PRODUCT_PACKAGES += \
+    power.universal7420 \
+    android.hardware.power@1.0-service \
+    android.hardware.power@1.0-impl
+
 # Ramdisk
 PRODUCT_PACKAGES += \
     fstab.samsungexynos7420 \
