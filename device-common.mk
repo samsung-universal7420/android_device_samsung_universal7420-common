e1e21bd94dd45195fae0646997ef35d446c997f8
Stricted <info@stricted.net>
2018-10-03 04:45:52 +0200

    universal7420: Update bsp config flags
    Change-Id: I48dae62d85a545aaf8626b38045db0dc71b4a3e5

--- device-common.mk
+++ device-common.mk
@@ -227,8 +227,8 @@ PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-impl
 
 # call Samsung LSI board support package
-$(call inherit-product, hardware/samsung_slsi-cm/exynos5/exynos5.mk)
-$(call inherit-product, hardware/samsung_slsi-cm/exynos7420/exynos7420.mk)
+$(call inherit-product, hardware/samsung_slsi/exynos5/exynos5.mk)
+$(call inherit-product, hardware/samsung_slsi/exynos7420/exynos7420.mk)
 
 # call the proprietary setup
 $(call inherit-product, vendor/samsung/universal7420-common/universal7420-common-vendor.mk)
