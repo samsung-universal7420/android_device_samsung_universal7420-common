e3398309aff9a6081e0702d555ea96eed6863576
Jan Altensen <info@stricted.net>
2019-07-10 03:24:25 +0200

    universal7420: Build new hidl based lights hal
    Change-Id: I9ef58054bf2eb44238db00e7814137f5c9b0a672

--- device-common.mk
+++ device-common.mk
@@ -105,9 +105,7 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    lights.universal7420 \
-    android.hardware.light@2.0-impl \
-    android.hardware.light@2.0-service
+    android.hardware.light@2.0-service.samsung
 
 # Livedisplay
 PRODUCT_PACKAGES += \
