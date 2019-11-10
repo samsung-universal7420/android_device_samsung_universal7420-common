6bf6bf5be7e7e511bce1a4e7619fb1d1d8d334ca
pythonlimited <leonardomeitz@gmail.com>
2019-11-10 16:53:32 +0100

    universal7420: use legacy wifi service
    Change-Id: I4bd1353a58ea7ba217104c6eee587c2074346d35

--- device-common.mk
+++ device-common.mk
@@ -302,9 +302,7 @@ PRODUCT_PACKAGES += \
     libwpa_client \
     wpa_supplicant \
     wpa_supplicant.conf \
-    android.hardware.wifi@1.0-service \
-    android.hardware.wifi@1.0 \
-    android.hardware.wifi@1.0-impl
+    android.hardware.wifi@1.0-service.legacy
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
